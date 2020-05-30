92c02a7575be981b196295abbbf1592a6035724d
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-05-30 09:18:36 +0200

    Head and Body dmg percent

--- MonsterForestDGX/Assets/MonsterHit.cs
+++ MonsterForestDGX/Assets/MonsterHit.cs
@@ -4,16 +4,19 @@ public class MonsterHit : MonoBehaviour
 {
     public MonsterHealth health;
     public MonsterHitType monsterHitType;
+    [Range(0, 200)]
+    public float dmgPercent = 100;
 
     public void TakeDamage(float dmg, ElementType magicType)
     {
+        float damage = dmg * dmgPercent / 100;
         if(monsterHitType == MonsterHitType.Body)
         {
-            health.TakeDamageBody(dmg, magicType);
+            health.TakeDamageBody(damage, magicType);
         }
         else
         {
-            health.TakeDamageHead(dmg, magicType);
+            health.TakeDamageHead(damage, magicType);
         }
     }
 }
